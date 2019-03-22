3d5b37c28968cdf2013710c1c8c14a4a2dae4055
locatw <locatw@users.noreply.github.com>
2019-03-22 16:19:47 +0900

    load access token of Fitbit API

--- FitbitDataExporter/FitbitDataExporter/Program.fs
+++ FitbitDataExporter/FitbitDataExporter/Program.fs
@@ -1,4 +1,18 @@
-﻿[<EntryPoint>]
+﻿open FSharp.Data
+open System.IO
+
+type Secret = JsonProvider<"""{"AccessToken": "token"}""">
+
+let loadSecret (filePath : string) =
+    use reader = new StreamReader(filePath)
+    let secretText = reader.ReadToEnd()
+    Secret.Parse(secretText)
+
+[<EntryPoint>]
 let main _ =
-    printfn "Hello World from F#!"
+    let secret = loadSecret @".\Secret.json"
+    let accessToken = secret.AccessToken
+
+    printfn "%A" accessToken
+
     0
